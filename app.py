dc5566020af0a8a1fa2201e659f5a3a8480613aa
Deekshita Saikia <deekshitasaikia@gmail.com>
2023-02-08 01:47:20 -0500

    updated port

--- app.py
+++ app.py
@@ -20,5 +20,4 @@ def get_flights():
     return jsonify(response.json())
 
 if __name__ == "__main__":
-    # app.run(debug=True)
-    app.run(debug=False)
+    app.run(host="0.0.0.0", port=8080, debug=True)
